053085c7465be2cdb61e201dade6b7871523cce0
ninekulay <74440281+ninekulay@users.noreply.github.com>
2021-05-05 21:48:08 +0700

    commit

---
+++ tasks/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
